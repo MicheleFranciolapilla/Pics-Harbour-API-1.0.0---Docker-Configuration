55ff118207c5fc1f57e8a314800a8bde52d1b095
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2024-06-29 09:58:20 +0200

    runApi.sh fixed to work with all bash versions

--- runApi.sh
+++ runApi.sh
@@ -10,7 +10,7 @@ while (( "$#" )); do
     # Effettua il 'case', inteso come 'switch' in Javascript, sul primo parametro (parametro corrente, poichè 'shift' rimuove il parametro in posizione 1 ad ogni ciclo).
     # Il 'case' viene eseguito sul parametro convertito in 'lowercase'
     # Il ';;' ha lo stesso effetto del 'break' in Javascript
-    case "${1,,}" in
+    case "$(echo $1 | tr '[:upper:]' '[:lower:]')" in
         noseed)
             SEED_DB="false"
             shift
